33678c718f5c28f89d56950e51d0c912cb92ce05
Idin Karuei <github@idin.net>
2025-02-23 22:50:29 -0500

    commit before AI deletes by mistake

--- build/lib/route/__init__.py
+++
@@ -1,4 +0,0 @@
-from .directory_tree import directory_tree
-from .setup_utils import get_post_install_command, get_post_develop_command
-
-__all__ = ["directory_tree", "get_post_install_command", "get_post_develop_command"]
